25a1f45beb741ffd320b92573d3c8aeeb64a5364
Ivansh96 <evgenijshavshin@yandex.ru>
2023-09-12 17:47:17 +0700

    csrf token

--- src/main/java/com/example/chatpetproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/chatpetproject/config/WebSecurityConfig.java
@@ -26,7 +26,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
